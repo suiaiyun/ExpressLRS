cf962fb4776719310f0e4f1c548d915e4f2f8691
kunlin <466066151@qq.com>
2024-11-14 14:37:04 +0800

    edit define

--- src/include/target/MLRS_TX.h
+++ src/include/target/MLRS_TX.h
@@ -15,21 +15,22 @@
 #define GPIO_PIN_BUSY           PB4
 #define GPIO_PIN_TX_ENABLE      PB12
 #define GPIO_PIN_RX_ENABLE      PB8
+
 #define GPIO_PIN_RCSIGNAL_RX    PA3  // UART2
 #define GPIO_PIN_RCSIGNAL_TX    PA2  // UART2
+
 #define GPIO_PIN_BUZZER         PB7
 #define GPIO_PIN_BUTTON         PA15 // active low
 #define GPIO_PIN_LED_RED        PC13 // Right Red LED
 #define GPIO_PIN_LED_GREEN      PC14 // Left Green LED
-// #define GPIO_LED_RED_INVERTED   1
-#define GPIO_LED_GREEN_INVERTED 1
-#define GPIO_PIN_DEBUG_RX       PA10 // UART1 (usb)
-#define GPIO_PIN_DEBUG_TX       PA9  // UART1 (usb)
+
+#define GPIO_PIN_DEBUG_RX       PB11 // UART3 (Bluetooth)
+#define GPIO_PIN_DEBUG_TX       PB10 // UART3 (Bluetooth)
 #define GPIO_PIN_UART2RX_INVERT PC15 // XOR chip
 #define GPIO_PIN_UART2TX_INVERT PB9 // XOR chip
 
 // Power output
 #define MinPower                PWR_10mW
-#define HighPower               PWR_100mW
-#define MaxPower                PWR_250mW
+#define HighPower               PWR_250mW
+#define MaxPower                PWR_500mW
 #define POWER_OUTPUT_VALUES     {-15,-11,-7,-1,6}
